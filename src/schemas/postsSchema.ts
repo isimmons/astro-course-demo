555f3e1060c6cdef666b57142a77b1d4c88ebe93
isimmons <isimmons33@gmail.com>
2023-12-29 13:11:15 -0500

    change image to cover and adjust schema to use image() helper

--- src/schemas/postsSchema.ts
+++ src/schemas/postsSchema.ts
@@ -1,4 +1,4 @@
-import { z } from "astro:content";
+import { z, type SchemaContext } from "astro:content";
 import { format } from "date-fns";
 
 export type PostCategories =
@@ -7,22 +7,23 @@ export type PostCategories =
   | "studio tips"
   | "career advice";
 
-const postSchema = z.object({
-  title: z
-    .string()
-    .min(3, { message: "A post title but be at least 3 characters." }),
-  author: z
-    .string()
-    .min(3, { message: "An authors name must be at least 3 characters." })
-    .max(50, {
-      message: "An authors name should be no more than 50 characters.",
+const postSchema = ({ image }: SchemaContext) =>
+  z.object({
+    title: z
+      .string()
+      .min(3, { message: "A post title but be at least 3 characters." }),
+    author: z
+      .string()
+      .min(3, { message: "An authors name must be at least 3 characters." })
+      .max(50, {
+        message: "An authors name should be no more than 50 characters.",
+      }),
+    categories: z.array(z.custom<PostCategories>()),
+    date: z.string().transform((str) => format(new Date(str), "MMMM d, yyyy")),
+    featured: z.boolean().default(false),
+    cover: image().refine((img) => img.width >= 1024, {
+      message: "Cover image must be at least 1024 pixels",
     }),
-  categories: z.array(z.custom<PostCategories>()),
-  date: z.string().transform((str) => format(new Date(str), "MMMM D, YYYY")),
-  featured: z.boolean().default(false),
-  image: z.string(),
-});
-
-export type PostSchema = z.infer<typeof postSchema>;
+  });
 
 export default postSchema;
