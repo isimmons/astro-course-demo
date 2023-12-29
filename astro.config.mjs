7bfe52eaa79a50f8f477b7e3342dcedf094f12bb
isimmons <isimmons33@gmail.com>
2023-12-28 23:20:14 -0500

    add spotlight integration to dev bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  integrations: [sentry(), spotlightjs()]
+});
