83f8a6291551e24d4e40ed929a6262954618cc6f
isimmons <isimmons33@gmail.com>
2023-12-31 00:49:01 -0500

    idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
   },
-  integrations: [sentry(), spotlightjs(), tailwind(), icon()]
+  integrations: [sentry(), spotlightjs(), tailwind(), icon()],
 });
