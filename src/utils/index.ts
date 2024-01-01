79f5cc6b1a0d34a7b16c832512739f0e9a079104
isimmons <isimmons33@gmail.com>
2024-01-01 04:49:32 -0500

    impliment category tags

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+import type { CollectionEntry } from "astro:content";
+
+export const getCategoriesFromPosts = (posts: CollectionEntry<"posts">[]) => {
+  const allCategories = posts.flatMap((p) => p.data.categories);
+  return Array.from(new Set(allCategories));
+};
