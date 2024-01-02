39d444073ec164c5051800a71bfd66a3248236df
isimmons <isimmons33@gmail.com>
2024-01-02 05:51:07 -0500

    add svelte contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import spotlightjs from "@spotlightjs/astro";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs(), tailwind(), icon()],
-  site: "https://rhythm.nation",
+  integrations: [sentry(), spotlightjs(), tailwind(), icon(), svelte()],
+  site: "https://rhythm.nation"
 });
