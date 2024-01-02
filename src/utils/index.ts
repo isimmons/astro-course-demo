ac6733868c34704aadcb8fe69a1c97ae8e1a0ed9
isimmons <isimmons33@gmail.com>
2024-01-02 17:36:08 -0500

    basic contact form is WIP, Needs endpoint

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,7 @@ export const getCategoriesFromPosts = (posts: CollectionEntry<"posts">[]) => {
   const allCategories = posts.flatMap((p) => p.data.categories);
   return Array.from(new Set(allCategories));
 };
+
+export const redirectTo = (path: string) => {
+  window.location.replace(path);
+};
