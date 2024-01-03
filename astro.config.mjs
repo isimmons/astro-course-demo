ed6a2ca9b303bd156fc439a0356920aecc9fe9c3
isimmons <isimmons33@gmail.com>
2024-01-03 13:45:58 -0500

    test sentry now on SSR setup vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,26 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
-const devIntegrations = [
-  sentry({
-    dsn: "https://920f25fcf2b10d5a9514fac38287ab10@o1078821.ingest.sentry.io/4506504598192128",
-    sourceMapsUploadOptions: {
-      project: "astro-course-demo",
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    },
-  }),
-  spotlightjs(),
-];
+
+const devIntegrations = [spotlightjs()];
 const isDev = process.env.NODE_ENV === "development" ? true : false;
 const extraIntegrations = isDev ? [...devIntegrations] : [];
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), svelte(), ...extraIntegrations],
+  integrations: [
+    tailwind(),
+    icon(),
+    svelte(),
+    sentry({
+      dsn: "https://920f25fcf2b10d5a9514fac38287ab10@o1078821.ingest.sentry.io/4506504598192128",
+      sourceMapsUploadOptions: {
+        project: "astro-course-demo",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    ...extraIntegrations,
+  ],
   site: "https://astro-course-demo-six.vercel.app",
   output: "server",
   adapter: vercel({
