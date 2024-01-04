506a7fdb79aa47c7e521f4ab4d35d789a7c3a633
isimmons <isimmons33@gmail.com>
2024-01-03 23:36:40 -0500

    sentry problems again

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,26 +6,23 @@ import icon from "astro-icon";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
-const devIntegrations = [spotlightjs()];
+const devIntegrations = [
+  spotlightjs(),
+  sentry({
+    dsn: "https://920f25fcf2b10d5a9514fac38287ab10@o1078821.ingest.sentry.io/4506504598192128",
+    sourceMapsUploadOptions: {
+      project: "astro-course-demo",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      telemetry: false,
+    },
+  }),
+];
 const isDev = process.env.NODE_ENV === "development" ? true : false;
 const extraIntegrations = isDev ? [...devIntegrations] : [];
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon(),
-    svelte(),
-    sentry({
-      dsn: "https://920f25fcf2b10d5a9514fac38287ab10@o1078821.ingest.sentry.io/4506504598192128",
-      sourceMapsUploadOptions: {
-        project: "astro-course-demo",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-        telemetry: false,
-      },
-    }),
-    ...extraIntegrations,
-  ],
+  integrations: [tailwind(), icon(), svelte(), ...extraIntegrations],
   site: "https://astro-course-demo-six.vercel.app",
   output: "server",
   adapter: vercel({
