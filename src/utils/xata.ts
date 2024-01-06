98bf208ec8d887d991e9c021eff02c0d459972fc
isimmons <isimmons33@gmail.com>
2024-01-05 20:04:30 -0500

    fix env's and use xata client util

--- src/utils/xata.ts
+++ src/utils/xata.ts
@@ -1,6 +1,6 @@
 // This would allow you to wrap the generated client from src/xata.ts with your own credentials
 import { XataClient } from "~/xata.ts";
 export const xata = new XataClient({
-  apiKey: process.env.XATA_API_KEY as string,
-  branch: process.env.XATA_BRANCH as string,
+  apiKey: import.meta.env.XATA_API_KEY,
+  branch: import.meta.env.XATA_BRANCH as string,
 });
