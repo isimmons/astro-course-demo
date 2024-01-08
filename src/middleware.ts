967d8e297863f14858557daacde745a9ce60d3aa
isimmons <isimmons33@gmail.com>
2024-01-08 15:51:18 -0500

    remove sentry middleware handler from the mix

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { sequence } from "astro:middleware";
-import * as Sentry from "@sentry/astro";
-
-export const onRequest = sequence(
-  Sentry.handleRequest(),
-  // Add your other handlers after Sentry.handleRequest()
-);
